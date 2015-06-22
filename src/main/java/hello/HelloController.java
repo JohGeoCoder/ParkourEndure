37e20c61fc82d630c6c53cf223e82def252211fd
John George <joecracko@gmail.com>
2015-06-22 15:07:35 -0400

    initial commit

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/")
+    public ModelAndView index() {
+    	ModelAndView mv = new ModelAndView();
+    	mv.setViewName("hello");
+    	return mv;
+    }
+    
+}
