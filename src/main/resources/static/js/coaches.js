7e3beb6b053e0efa01ebe72c366fd738061f0fce
John George <joecracko@gmail.com>
2015-07-14 16:36:04 -0400

    Implemented Angular for the display of coaches.

--- src/main/resources/static/js/coaches.js
+++ src/main/resources/static/js/coaches.js
@@ -13,4 +13,22 @@
 		header.init();
 	});
 	
+	var app = angular.module('myApp', []);
+	app.controller('coachController', function($scope) {
+		$scope.selected = 0;
+		
+		$scope.setSelected = function(selected) {
+			$scope.selected = selected;
+		}
+		
+	    $scope.coaches = [
+	        {
+	        	firstName:'John', 
+	        	lastName:'George',
+	        	imageUrl:'img/pkCoachJohn.png',
+	        	details:'John began practicing parkour in 2005 when parkour coaching in the United States was almost non-existent. He began coaching in 2009 during his time at Bloomsburg University of Pennsylvania, and earned his Level 1 ADAPT coaching qualification in 2011. In 2015 John took the intensive 5-day Level 2 ADAPT coaching qualification, and is actively training for the Level 2 assessments.'
+	        }
+	    ];
+	});
+	
 })();
