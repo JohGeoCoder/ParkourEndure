9ef8beec835b0edb37be5a7b9bc4890ed36089dd
John George <joecracko@gmail.com>
2015-07-16 16:58:22 -0400

    Attempting to move carousel information to the back end being called by angular.

---
+++ src/main/java/models/CarouselItem.java
@@ -0,0 +1,33 @@
+package models;
+
+public class CarouselItem {
+	private String type;
+	private String quote;
+	private String attribution;
+	private String imageUrl;
+	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getQuote() {
+		return quote;
+	}
+	public void setQuote(String quote) {
+		this.quote = quote;
+	}
+	public String getAttribution() {
+		return attribution;
+	}
+	public void setAttribution(String attribution) {
+		this.attribution = attribution;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+}
--- src/main/resources/static/js/imageCarousel.js
+++ src/main/resources/static/js/imageCarousel.js
@@ -1,12 +1,7 @@
 (function() {
 	var imageCarousel = function(){
 		var init = function(){
-			$('.image-carousel').slick({
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				autoplay: true,
-				autoplaySpeed: 5000,
-			});
+			var i = 1;
 		};
 		
 		return{
@@ -15,29 +10,26 @@
 		
 	};
 	
+	ParkourEndureApp.controller('carouselController', function($scope, $http){
+		$http.post('/carouselimages')
+		.success(function(data, status, headers, config){
+			$scope.carouselItems = data;
+		})
+		.error(function(data, status, headers, config){
+			$scope.carouselItems = [];
+		});
+	});
+	
 	$(function() {
 		ImageCarousel = imageCarousel();
 		ImageCarousel.init();
 	});
 	
-	ParkourEndureApp.controller('carouselController', function($scope){
-		$scope.carouselItems = [
-		    {
-		    	type:'Q',
-		    	quote:"You don't stop running because you get old, you get old because you stop running.",
-		    	attribution:'Christopher McDougall'
-		    },
-		    {
-		    	type:'Q',
-		    	quote:"Once you learn Parkour's basic moves, the world around you changes",
-		    	attribution:'Christopher McDougall'
-		    },
-		    {
-		    	type:'Q',
-		    	quote:"Whether you're a beginner or have been in the trenches for years, your primary goal as far as physical training goes should be to become stronger.",
-		    	attribution:'Chris Rowat'
-		    },
-		];
-	});
-	
 })();
+
+$('.image-carousel').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplay: true,
+	autoplaySpeed: 5000,
+});
