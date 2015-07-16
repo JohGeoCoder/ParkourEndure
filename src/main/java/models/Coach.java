2fa069d3a7b589f598c98148cd83579ace6f14a8
John George <joecracko@gmail.com>
2015-07-16 12:44:45 -0400

    coaches data is now grabbed from the back end.

---
+++ src/main/java/models/Coach.java
@@ -0,0 +1,33 @@
+package models;
+
+public class Coach {
+	private String firstName;
+	private String lastName;
+	private String imageUrl;
+	private String details;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	public String getDetails() {
+		return details;
+	}
+	public void setDetails(String details) {
+		this.details = details;
+	}
+}
--- src/main/resources/static/js/coaches.js
+++ src/main/resources/static/js/coaches.js
@@ -13,17 +13,18 @@
 		header.init();
 	});
 	
-	ParkourEndureApp.controller('coachController', function($scope) {
+	ParkourEndureApp.controller('coachController', function($scope, $http) {
 		$scope.selected = {index:0};
 		
-	    $scope.coaches = [
-	        {
-	        	firstName:'John', 
-	        	lastName:'George',
-	        	imageUrl:'img/pkCoachJohn.png',
-	        	details:'John began practicing parkour in 2005 when parkour coaching in the United States was almost non-existent. He began coaching in 2009 during his time at Bloomsburg University of Pennsylvania, and earned his Level 1 ADAPT coaching qualification in 2011. In 2015 John took the intensive 5-day Level 2 ADAPT coaching qualification, and is actively training for the Level 2 assessments.'
-	        }
-	    ];
+		$http.post('/coaches')
+		.success(function(data, status, headers, config){
+			$scope.coaches = data;
+		})
+		.error(function(data, status, headers, config){
+			$scope.coaches = [];
+		});
+		
+		var i = 10;
 	});
 	
 })();
