93b66b448ae3a4893225183f417480cbdbb48470
John George <joecracko@gmail.com>
2015-07-17 11:19:00 -0400

    The image carousel runs Slick after Angular renders the page.

--- src/main/resources/static/js/imageCarousel.js
+++ src/main/resources/static/js/imageCarousel.js
@@ -1,23 +1,37 @@
 (function() {
 	var imageCarousel = function(){
 		var init = function(){
-			var i = 1;
+			
 		};
 		
 		return{
 			init:init
 		};
-		
 	};
 	
-	ParkourEndureApp.controller('carouselController', function($scope, $http){
+	ParkourEndureApp.controller('carouselController', function($scope, $http, $timeout){
+		$scope.carouselItems = [];
+		
 		$http.post('/carouselimages')
 		.success(function(data, status, headers, config){
 			$scope.carouselItems = data;
+			
+			//This is required to run Slick after Angular renders the view.
+			$timeout(function () { $scope.runSlickOnCarousel(data); }, 0); //0ms timeout
 		})
 		.error(function(data, status, headers, config){
 			$scope.carouselItems = [];
 		});
+		
+		$scope.runSlickOnCarousel = function()
+	    {
+			$('.image-carousel').slick({
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				autoplay: true,
+				autoplaySpeed: 5000,
+			});
+	    };
 	});
 	
 	$(function() {
@@ -26,10 +40,3 @@
 	});
 	
 })();
-
-$('.image-carousel').slick({
-	slidesToShow: 1,
-	slidesToScroll: 1,
-	autoplay: true,
-	autoplaySpeed: 5000,
-});
